356b1f0f902bb94adc512c308e0e4f903c72134d
i514097 <tianli.gao@sap.com>
2021-01-30 14:33:10 -0500

    fix image url

--- src/main/resources/db/migration/V2_0__add_order_product_table.sql
+++ src/main/resources/db/migration/V2_0__add_order_product_table.sql
@@ -1,5 +1,8 @@
 insert into store_products (id, name, price, url)
 values
-('0100355f-d0fe-49a4-b017-8fc568cbc87f', 'product1', 2.99, 'https://www.google.com'),
-('2a00355f-d0fe-49a4-b017-8fc568cbc12b', 'product2', 217.99, 'https://www.google.com'),
-('3c00355f-d0fe-49a4-b017-8fc568cbb017', 'product3', 54.99, 'https://www.google.com');
+('0100355f-d0fe-49a4-b017-8fc568cbc87f', 'iPhone 12 PRO 256G Black', 1399.00, 'https://i.ibb.co/TrkY1By/iphone.jpg'),
+('2a00355f-d0fe-49a4-b017-8fc568cbc12b', 'Sumsang TV 4K 100 inch', 2217.00, 'https://i.ibb.co/4t8qgvx/tv.jpg'),
+('1b00325f-d0fe-49a4-b017-8fc568cbc12b', 'Leather Sofa', 992.00, 'https://i.ibb.co/HGT2C5h/sofa.jpg'),
+('9b03025f-d0fe-49a4-b017-8fc568cbc12b', 'Bed Frame', 389.99, 'https://i.ibb.co/vw8Yscj/bed.jpg'),
+('1e00325f-d0fe-49a4-b017-8fc568cbc12b', 'Ice Cream', 8.99, 'https://i.ibb.co/BB5xWPJ/icecream.jpg'),
+('4a66325f-d0fe-49a4-b017-8fc568cbb017', 'Tablet Cover', 54.99, 'https://i.ibb.co/mz85gyD/cover.jpg');
